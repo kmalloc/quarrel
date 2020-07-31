3c5699e403e98333bd296505195281ea0935731b
miliao <miliao@tencent.com>
2020-07-31 18:45:59 +0800

    draft

--- test/test_waitgroup.cpp
+++ test/test_waitgroup.cpp
@@ -13,17 +13,26 @@ TEST(waitgroup, testwg) {
     std::thread t1([&]() { wg.Notify(); });
     std::thread t2([&]() { wg.Notify(); });
 
-    // auto tm = std::chrono::milliseconds(10);
-
     ASSERT_FALSE(wg.Wait(3));
 
     std::thread t3([&]() { wg.Notify(); });
 
     ASSERT_TRUE(wg.Wait(3));
+    ASSERT_TRUE(wg.Wait(3));
+
+    std::thread t4([&]() { wg.Notify(); });
+
+    auto start = std::chrono::steady_clock::now();
+    ASSERT_TRUE(wg.Wait(3));
+    auto end = std::chrono::steady_clock::now();
+
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    ASSERT_LT(diff, 3);
 
     t1.join();
     t2.join();
     t3.join();
+    t4.join();
 }
 
 TEST(waitgroup, testwgtimeout) {
